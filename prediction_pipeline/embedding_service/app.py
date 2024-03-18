b9569bba635edce9c06079fc1ea8253cabb8fc53
Shubham <subham.k.ojha@gmail.com>
2024-03-19 02:07:42 +0530

    Add app.py for embedding service

---
+++ prediction_pipeline/embedding_service/app.py
@@ -0,0 +1,53 @@
+import os
+import torch
+from torchvision import transforms
+from efficientnet_pytorch import EfficientNet
+import faiss
+from PIL import Image
+import numpy as np
+from io import BytesIO
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+model = EfficientNet.from_pretrained('efficientnet-b7')
+model.eval() # setting to eval mode
+
+preprocess = transforms.Compose([
+    transforms.Resize(256),
+    transforms.CenterCrop(224),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+])
+
+# index = faiss.IndexFlatL2(model.classifier.out_features)
+index = faiss.IndexFlatL2(model._fc.in_features)
+embeddings = []
+
+def extract_embeddings(image):
+    with torch.no_grad():
+        input_tensor = preprocess(image).unsqueeze(0)
+        embedding = model.extract_features(input_tensor).cpu().numpy().flatten()
+    return embedding
+
+@app.route('/generate_embedding', methods=['POST'])
+def generate_embedding():
+    image_file = request.files['image']
+    # image_path = os.path.join('images', image.filename)
+    # image = Image.open(image_file)
+    # image.save(image_path)
+    image_bytes = image_file.read()
+    image_stream = BytesIO(image_bytes)
+    image_stream.seekable = True
+    image = Image.open(image_stream)
+
+    embedding = extract_embeddings(Image.open(image))
+    embeddings.append(embedding)
+    index.add(np.array(embedding))
+    
+    return jsonify({'embedding':embedding.tolist()})
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
+
