2746c4427b2e8e2acc94e5c9f72ce73e950bf950
Shubham <subham.k.ojha@gmail.com>
2024-03-20 16:16:02 +0530

    Added test for valid image file

--- tests/test_embedding_service.py
+++ tests/test_embedding_service.py
@@ -9,4 +9,18 @@ from io import BytesIO
 from prediction_pipeline.embedding_service.app import app, extract_embeddings
 
 class TestEmbeddingService(unittest.TestCase):
-    pass
+    def setUp(self):
+        self.app = app.test_client()
+        self.app.testing = True
+
+    def test_generate_embedding(self):
+        # To test with a valid image file
+        with open('tests/test_image.jpg', 'rb') as f:
+            data = dict(image=(f, 'test_image.jpg'))
+            response = self.app.post('/generate_embedding', data=data, content_type='multipart/form-data')
+            self.assertEqual(response.status_code, 200)
+            data = json.loads(response.data)
+            self.assertIsInstance(data['embedding'], list)
+            self.assertGreater(len(data['embedding']), 0)
+            
+
