a8b6d286f58ba0e83c2b2347382482542b9ddec3
Shubham <subham.k.ojha@gmail.com>
2024-03-28 11:36:04 +0530

    MinIO client works

--- indexing_pipeline/image_storage/object_storage_client.py
+++ indexing_pipeline/image_storage/object_storage_client.py
@@ -1,5 +1,6 @@
 from minio import Minio
-from minio.error import ResponseError
+from minio.error import InvalidResponseError
+import os
 
 # Create a MinIO client
 client = Minio(
@@ -8,3 +9,24 @@ client = Minio(
     secret_key="minioadmin",
     secure=False,
 )
+# print(os.getcwd())
+
+base_dir = './common/storage'
+data_dirs = [os.path.join(base_dir, "data1"), os.path.join(base_dir, "data2"), os.path.join(base_dir, "data3")]
+erasure_code_config = {"data": 2, "parity": 1}
+
+# print(data_dirs)
+
+bucket_name = "images"
+try:
+    if not client.bucket_exists(bucket_name):
+        client.make_bucket(bucket_name, erasure_code="myerasureset")
+except Exception as err:
+    pass  # Bucket already exists
+except InvalidResponseError as err:
+    print(err)
+
+
+# image_path = ''
+# object_name = ''
+
