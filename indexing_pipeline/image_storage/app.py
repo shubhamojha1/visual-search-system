ac91b9071b55a4c17c05b6769588cea110fe3e31
Shubham <subham.k.ojha@gmail.com>
2024-03-30 02:57:38 +0530

    Fix file upload integration by correctly sending FileStorage object to embedding generation service

--- indexing_pipeline/image_storage/app.py
+++ indexing_pipeline/image_storage/app.py
@@ -2,9 +2,13 @@ from flask import Flask, request, jsonify
 from bucket_management import create_bucket
 from object_storage import store_object, retrieve_object
 from minio import Minio
+import base64
+import requests
 
 
 app = Flask(__name__)
+
+EMBEDDING_GENERATION_SERVICE_URL = 'http://localhost:7000/generate_embedding'
 
 client = Minio(
     "localhost:9000",
@@ -24,24 +28,33 @@ def upload_image():
         return jsonify({"error": "No selected file"}), 400
     if file:
         create_bucket(client, BUCKET_NAME)
-        print(file)
+        print("file: ", file)
         try:
             file_content = file.read() # 
             # print(file_content)
             # print(file.filename)
             store_object(client, BUCKET_NAME, file.filename, file_content)#file)
-            return jsonify({"success": "Image uploaded successfully"}), 200
+
+            image_data = base64.b64encode(file_content).decode('utf-8')
+            response = requests.post(EMBEDDING_GENERATION_SERVICE_URL, 
+                                     files={
+                                         'image': (file.filename, file.stream, file.content_type) # do not send 'file' object alone. WORKS THIS WAY ONLY
+                                                                                                    # need to send other metadata as well. 
+                                     })
+            print(response)
+            print(response.json())
+            if response.status_code == 200:
+                embedding = response.json()
+                # save embedding to index table
+                print(embedding)
+                return jsonify({"success": "Image uploaded and embedding generated successfully", "embedding": embedding}), 200 
+            else:
+                return jsonify({"error": "Failed to generate embedding"}), 500
+            # return jsonify({"success": "Image uploaded successfully"}), 200
         except Exception as err:
             return jsonify({"error": str(err)}), 500
     return jsonify({"error": "Unknown error"}), 500
 
-# @app.route('/retrieve/<image_name>', methods=['GET'])
-# def retrieve_image(image_name):
-#     try:
-#         data = retrieve_object(client, BUCKET_NAME, image_name, "retrieved_image.jpg")
-#         return data, 200
-#     except Exception as err:
-#         return jsonify({"error": str(err)}), 500
 @app.route('/retrieve/<image_name>', methods=['GET'])
 def retrieve_image(image_name):
     try:
@@ -54,4 +67,4 @@ def retrieve_image(image_name):
 
     
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=8889)
