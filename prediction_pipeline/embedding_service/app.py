c6a3de75041f86da53bd138fc8bec9a5e22166d0
Shubham <subham.k.ojha@gmail.com>
2024-04-16 01:48:25 +0530

    Add monkey patch for imghdr to validate .jpg files

--- prediction_pipeline/embedding_service/app.py
+++ prediction_pipeline/embedding_service/app.py
@@ -87,8 +87,9 @@ def generate_embedding():
         if not allowed_file(image_file.filename):
             print(image_file.filename)
             return jsonify({'error': 'Invalid file type'}), 400
-        
+        # print(imghdr.what(image_file))
         if imghdr.what(image_file) is None:
+            # if os.path.splitext(image_file)[1].lower() != '.jpg': # imghdr doesnt support detection of 'jpg', so added explicitly
             return jsonify({'error': 'Invalid image file'}), 400
         # image_path = os.path.join('images', image.filename)
         # image = Image.open(image_file)
@@ -113,6 +114,36 @@ def generate_embedding():
     return jsonify({'embedding':embedding_list})
 
 
+def monkey_patch_imghdr():
+    # Monkeypatch bug in imagehdr
+    from imghdr import tests
+
+    def test_jpeg1(h, f):
+        """JPEG data in JFIF format"""
+        if b'JFIF' in h[:23]:
+            return 'jpeg'
+
+
+    JPEG_MARK = b'\xff\xd8\xff\xdb\x00C\x00\x08\x06\x06' \
+                b'\x07\x06\x05\x08\x07\x07\x07\t\t\x08\n\x0c\x14\r\x0c\x0b\x0b\x0c\x19\x12\x13\x0f'
+
+    def test_jpeg2(h, f):
+        """JPEG with small header"""
+        if len(h) >= 32 and 67 == h[5] and h[:32] == JPEG_MARK:
+            return 'jpeg'
+
+
+    def test_jpeg3(h, f):
+        """JPEG data in JFIF or Exif format"""
+        if h[6:10] in (b'JFIF', b'Exif') or h[:2] == b'\xff\xd8':
+            return 'jpeg'
+
+    tests.append(test_jpeg1)
+    tests.append(test_jpeg2)
+    tests.append(test_jpeg3)
+
+
 if __name__ == '__main__':
+    monkey_patch_imghdr()
     app.run(debug=True, host='0.0.0.0', port=7000)
 
